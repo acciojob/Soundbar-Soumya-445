be6d32b735f7e80587e80fa4452f9d61e7c40a5f
Soumya Manasa Basantaray <soumyamanasa2003@gmail.com>
2026-03-02 19:38:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+const buttons = document.querySelectorAll(".btn");
+const stopBtn = document.querySelector(".stop");
+
+let currentAudio = null;
+
+// Play sound
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const soundName = button.getAttribute("data-sound");
+
+    // Stop previous sound if playing
+    if (currentAudio) {
+      currentAudio.pause();
+      currentAudio.currentTime = 0;
+    }
+
+    currentAudio = new Audio(`sounds/${soundName}.mp3`);
+    currentAudio.play();
+  });
+});
+
+// Stop button
+stopBtn.addEventListener("click", () => {
+  if (currentAudio) {
+    currentAudio.pause();
+    currentAudio.currentTime = 0;
+  }
+});
